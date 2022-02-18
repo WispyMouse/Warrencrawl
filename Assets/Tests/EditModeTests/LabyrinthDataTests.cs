54f108c50ae852e21f33b3688a0d41e7a1e54892
Mint <wispymouse@gmail.com>
2022-02-18 13:36:49 -0800

    Additional LabyrinthLevel Tests
    NeighborsOfCoordinate_ReturnsEmpty
    NeighborsOfCoordinate_ReturnsEmptyForExisting
    NeighborsOfCoordinate_ReturnsExpectedCoordinates

--- Assets/Tests/EditModeTests/LabyrinthDataTests.cs
+++ Assets/Tests/EditModeTests/LabyrinthDataTests.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
@@ -30,14 +31,59 @@ public class NewTestScript
     }
 
     /// <summary>
-    /// When a LabyrinthLevel data structure is created, is it safe to get null?
+    /// When a LabyrinthLevel is created, is it safe to get null?
     /// </summary>
     [Test]
-    public void LabyrinthLevel_CanGetNull()
+    public void LabyrinthLevel_CellAtCoordinate_CanGetNull()
     {
         LabyrinthLevel testLevel = new LabyrinthLevel();
 
         Assert.That(testLevel.CellAtCoordinate(CellCoordinates.Zero), Is.Null);
         Assert.That(testLevel.Cells, Is.Empty);
+    }
+
+    /// <summary>
+    /// When a LabyrinthLevel is created, is it safe to get the neighbors of a missing coordinate?
+    /// </summary>
+    [Test]
+    public void LabyrinthLevel_NeighborsOfCoordinate_ReturnsEmpty()
+    {
+        LabyrinthLevel testLevel = new LabyrinthLevel();
+
+        Assert.That(testLevel.NeighborsOfCoordinate(CellCoordinates.Zero), Is.Empty);
+    }
+
+    /// <summary>
+    /// When a LabyrinthLevel is created, is it safe to get the neighbors of its only cell? Is the list of neighbors empty?
+    /// </summary>
+    [Test]
+    public void Labyrinthlevel_NeighborsOfCoordinate_ReturnsEmptyForExistingCoordinate()
+    {
+        LabyrinthLevel testLevel = new LabyrinthLevel();
+
+        testLevel.Cells.Add(new LabyrinthCell() { Coordinate = CellCoordinates.Zero });
+
+        Assert.That(testLevel.NeighborsOfCoordinate(CellCoordinates.Zero), Is.Empty);
+    }
+
+    /// <summary>
+    /// When a LabyrinthLevel is created, are neighbors returned as expected through NeighborsOfCoordinate?
+    /// </summary>
+    [Test]
+    public void Labyrinthlevel_NeighborsOfCoordinate_ReturnsExpectedCoordinates()
+    {
+        LabyrinthLevel testLevel = new LabyrinthLevel();
+
+        testLevel.Cells.Add(new LabyrinthCell() { Coordinate = CellCoordinates.Zero });
+        testLevel.Cells.Add(new LabyrinthCell() { Coordinate = new CellCoordinates(1, 0, 0) });
+        testLevel.Cells.Add(new LabyrinthCell() { Coordinate = new CellCoordinates(1, 1, 0) });
+
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 0, 0)), Does.Contain(new CellCoordinates(1, 1, 0)));
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 0, 0)), Does.Contain(new CellCoordinates(0, 0, 0)));
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 0, 0)).Count(), Is.EqualTo(2));
+
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 1, 0)), Does.Contain(new CellCoordinates(1, 0, 0)));
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 1, 0)), Does.Not.Contains(new CellCoordinates(0, 0, 0)));
+        Assert.That(testLevel.NeighborsOfCoordinate(new CellCoordinates(1, 1, 0)).Count(), Is.EqualTo(1));
     }
 }
