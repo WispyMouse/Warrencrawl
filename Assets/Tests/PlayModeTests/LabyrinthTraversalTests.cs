74f384a973f8bc10661b0c1b95a53dfba844263c
Mint <wispymouse@gmail.com>
2022-02-28 13:29:39 -0800

    Couple more Traversal tests
    - Rotate_CanRotateAround
    - Step_CanStepBackward

--- Assets/Tests/PlayModeTests/LabyrinthTraversalTests.cs
+++ Assets/Tests/PlayModeTests/LabyrinthTraversalTests.cs
@@ -64,6 +64,78 @@ public class LabyrinthTraversalTests
         Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, 0, 0)));
     }
 
+    /// <summary>
+    /// Validates that the player can rotate all the way to the right and all the way to the left, and that the directions being faced are as expected.
+    /// </summary>
+    [UnityTest]
+    public IEnumerator Rotate_CanRotateAround()
+    {
+        GlobalStateMachine stateMachine = new GlobalStateMachine(new WarrencrawlInputs());
+
+        LabyrinthState state = new LabyrinthState(new TestLabyrinthProvider());
+
+        yield return stateMachine.ChangeToState(state);
+
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.North));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.East));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.South));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.West));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.North));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateLeft());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.West));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateLeft());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.South));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateLeft());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.East));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateLeft());
+        Assert.That(state.PointOfViewInstance.CurFacing, Is.EqualTo(Direction.North));
+    }
+
+    /// <summary>
+    /// Asserts that you can take a step backward when intended.
+    /// </summary>
+    [UnityTest]
+    public IEnumerator Step_CanStepBackward()
+    {
+        GlobalStateMachine stateMachine = new GlobalStateMachine(new WarrencrawlInputs());
+
+        LabyrinthState state = new LabyrinthState(new TestLabyrinthProvider());
+
+        yield return stateMachine.ChangeToState(state);
+
+        yield return state.Step(state.PointOfViewInstance.CurFacing.Backward());
+        Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, -1, 0)));
+
+        // Should fail to move in to non-walkable space
+        yield return state.Step(state.PointOfViewInstance.CurFacing.Backward());
+        Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, -1, 0)));
+
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+        yield return state.Rotate(state.PointOfViewInstance.CurFacing.RotateRight());
+
+        yield return state.Step(state.PointOfViewInstance.CurFacing.Backward());
+        Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, 0, 0)));
+
+        yield return state.Step(state.PointOfViewInstance.CurFacing.Backward());
+        Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, 1, 0)));
+
+        // Should fail to move in to non-walkable space
+        yield return state.Step(state.PointOfViewInstance.CurFacing.Backward());
+        Assert.That(state.PointOfViewInstance.CurCoordinates, Is.EqualTo(new CellCoordinates(0, 1, 0)));
+    }
+
     class TestLabyrinthProvider : IGameLevelProvider
     {
         public GameLevel GetLevel()
@@ -72,6 +144,8 @@ public class LabyrinthTraversalTests
             testLevel.LabyrinthData = new LabyrinthLevel();
             testLevel.LabyrinthData.Cells = new List<LabyrinthCell>()
             {
+                new LabyrinthCell() { Coordinate = new CellCoordinates(0, -2, 0), Walkable = false },
+                new LabyrinthCell() { Coordinate = new CellCoordinates(0, -1, 0), Walkable = true },
                 new LabyrinthCell() { Coordinate = new CellCoordinates(0, 0, 0), Walkable = true },
                 new LabyrinthCell() { Coordinate = new CellCoordinates(0, 1, 0), Walkable = true },
                 new LabyrinthCell() { Coordinate = new CellCoordinates(0, 2, 0), Walkable = false },
